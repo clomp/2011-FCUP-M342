4e8804215cc3d279dd57a50b7cbdeb93ef5862bf
clomp <clomp@asrock.(none)>
2011-09-12 11:08:02 +0100

    t

--- c++/first.cpp
+++ c++/first.cpp
@@ -4,5 +4,9 @@ using namespace std;
 int main()
 {
 	cout << "Olá mundo!" << endl;
+	cout << "Dois numeros:" ;
+	unsigned int a,b;
+	cin >> a >> b;
+	cout << "A soma de "<< a << " e " << b <<" é " << a+b << endl;
 	return 0;
 }
